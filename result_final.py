e549fe6d469b06505feff04bec9fcb679076665d
ASHUTOSH KUSHWAHA <credibleashu@gmail.com>
2017-05-05 08:47:49 -0500

    Update result_final.py

--- result_final.py
+++ result_final.py
@@ -1,24 +1,27 @@
 from selenium import webdriver
 browser = webdriver.Firefox()
-#print("1")
+
 browser.get("http://dolphintechnologies.in/manit/results.html")
-#print("2")
+
 schno=browser.find_element_by_id("scholar")
 type(schno)
-#print("3")
-sem=browser.find_element_by_id("semester")#print("4")
+
+sem=browser.find_element_by_id("semester")
 submit=browser.find_element_by_id("button")
-#print("5")
+
 file = open("Result.txt",'w')
 count=1
-for i in range(141112001,141112051):
+start_num= 141112001
+end_num = 141112051
+sem_num = 3
+for i in range(start_num,end_num):
     try:
         t=''
         schno=browser.find_element_by_id("scholar")
         schno.click()
         schno.send_keys(i)
         sem=browser.find_element_by_id("semester")
-        sem.send_keys('3')
+        sem.send_keys(str(sem_num))
         submit=browser.find_element_by_id("button")
         submit.click()
         for x in browser.find_elements_by_class_name('style16'):
@@ -45,7 +48,7 @@ for i in range(141112001,141112051):
         count = count + 1
         print("Error : ",ex ," at " ,i)
         browser.get("http://dolphintechnologies.in/manit/results.html")
-        if(count==10):
+        if(count==10): # if 10 errors have occurred
             file.close()
             break;
     #browser.back()
